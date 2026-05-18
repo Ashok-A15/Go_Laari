160bb1c674e65dc4e41774e58ce31b8213d1f167
Ashok-A15 <mrashok067@gmail.com>
2026-05-18 17:53:06 +0530

    Force Legacy Google Maps Renderer in Android MainActivity to fix blank/green map rendering bug on budget devices like A015

--- golorry_owner_driver_app_master/android/app/src/main/kotlin/com/example/flutter_project_1/MainActivity.kt
+++ golorry_owner_driver_app_master/android/app/src/main/kotlin/com/example/flutter_project_1/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.example.flutter_project_1
 
+import android.os.Bundle
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.MapsInitializer.Renderer
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Force the Legacy Google Maps Renderer to prevent blank/green screen crashes on budget devices like A015
+        MapsInitializer.initialize(applicationContext, Renderer.LEGACY, object : OnMapsSdkInitializedCallback {
+            override fun onMapsSdkInitialized(renderer: Renderer) {
+                when (renderer) {
+                    Renderer.LATEST -> android.util.Log.d("MapsRenderer", "The latest version of the renderer is used.")
+                    Renderer.LEGACY -> android.util.Log.d("MapsRenderer", "The legacy version of the renderer is used.")
+                }
+            }
+        })
+    }
+}
